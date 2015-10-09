55971d1480dc9cebebcfa54f68479365c6fe4f8c
weiqiang3 <weiqiang3@staff.weibo.com>
2015-10-09 18:44:28 +0800

    modify factory pattern

--- src/factorypattern/ShapeFactory.java
+++ src/factorypattern/ShapeFactory.java
@@ -4,6 +4,7 @@ package factorypattern;
  * Created by weiqiang3 on 15-10-9.
  */
 public class ShapeFactory {
+    //客户端不知道返回的具体类型
     public Shape getShape(String shapeType) {
         if (shapeType == null) {
             return null;
